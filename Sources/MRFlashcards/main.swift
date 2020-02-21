db630a367ba52c02501af678aacb7d7785fcb77f
Margot <rasamymargot@gmail.com>
2020-02-21 22:47:25 +0100

    Javascript file linked

--- Sources/MRFlashcards/main.swift
+++ Sources/MRFlashcards/main.swift
@@ -8,6 +8,7 @@ HeliumLogger.use()
 let newFlashcardsGame = MRFlashcardsGame()
 newFlashcardsGame.createDecksFromCsv(csvFolderPath : "./Sources/MRFlashcards/assets/csv")
 print(newFlashcardsGame.allDecks)
+let decks = newFlashcardsGame.allDecks 
 
 
 
@@ -15,17 +16,14 @@ let router = Router()
 router.all(middleware: [BodyParser(), StaticFileServer(path: "./Public")])
 router.add(templateEngine: StencilTemplateEngine())
 
-let decks = newFlashcardsGame.allDecks 
-
 
 router.get("/") { request, response, next in
-  // var hello : String = ""
-  // response.send(newFlashcardsGame.allDecks)
-  // try response.render("Home.stencil", context : [hello : ""])
-
   try response.render("Home.stencil", with: decks, forKey: "decks")
   next()
  }
+
+
+
 
 
  Kitura.addHTTPServer(onPort: 8080, with: router)
