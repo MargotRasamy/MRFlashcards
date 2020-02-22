9ead646aa2cad83171a433cc386c41d964ef7a9c
Margot <rasamymargot@gmail.com>
2020-02-22 01:17:57 +0100

    Almost done - Mapping in stencil to be fixed and score to be added

--- Public/scripts/script.js
+++ Public/scripts/script.js
@@ -1,9 +1,12 @@
 let decks = document.querySelectorAll('.deck')
+let cardQuestion = document.querySelectorAll('.cardQuestion')
+let cardResponse = document.querySelectorAll('.cardResponse')
+
 var subjectName
 var subjectNameTrimed // No white space for path
 
-
-for(let i = 0; i<decks.length ; i++)
+//etape 1 click pour redirection vers les routes
+for(let i = 0; i<decks.length ; i++){
 decks[i].addEventListener(
     
     'click',
@@ -12,4 +15,21 @@ decks[i].addEventListener(
         subjectNameTrimed = subjectName.replace(/\s/gi,'')
         window.location.href="/matiere/" + subjectNameTrimed
     }
-)
+
+    )
+
+    
+}
+
+
+//2eme etape afficher et cacher les reponses
+for(let j = 0; j < cardQuestion.length; j++){
+    cardQuestion[j].addEventListener(
+        'click',
+        function(){
+           
+            cardResponse[j].classList.toggle("is-hidden")
+        }
+    )
+
+}
